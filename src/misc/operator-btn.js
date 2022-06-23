4ee0df928a90a2f444dcd27ee9395f52935c6d0f
guhaiye <799677429@qq.com>
2022-06-23 17:32:57 +0800

    脚手架更新

--- src/misc/operator-btn.js
+++ src/misc/operator-btn.js
@@ -1,15 +1,23 @@
-import {Dropdown,DropdownMenu} from 'view-design'
 /**
  * @param {array} arr :重新生成html
  * @param {function} h:vue的h函数
  * description：将操作重新生成页面元素，数组有三个就直接显示数组，超过三个，生成更多下拉，显示其他的操作 
  */
+import {Dropdown,DropdownMenu,DropdownItem} from 'view-design'
 export function operatorBtn(arr,h){
     let operator_arr = []
     if(arr.length > 3){
         let new_arr_before = arr.slice(0,2);
         let new_arr_after = arr.slice(2);
         operator_arr.push(new_arr_before)
+        var newAfter = []
+        new_arr_after.forEach(e => {
+            newAfter.push(h(DropdownItem,{
+                attrs:{
+                    class:'btnlist_content'
+                }
+            },[e]))
+        })
         let dropdown_ele = h(Dropdown,{
             attrs:{
               placement:'top-end',
@@ -29,11 +37,7 @@ export function operatorBtn(arr,h){
             ]),
             h(DropdownMenu,{
              slot:'list'  
-            },[h('div',{
-                attrs:{
-                    class:'btnlist_content'
-                }
-            },new_arr_after)])
+            },newAfter)
         ])
         operator_arr.push(dropdown_ele)
      }else{
